53bd09a11731c5fc2fb7572ecbe5697f03a502d5
AniediAbasi Udoh <AniediAbasi.Udoh@sterling.ng>
2024-12-31 21:00:15 +0100

    context

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:SportBetConnection"]
+    );
+});
 
 var app = builder.Build();
 
--- SportsSln/SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
+++ SportsSln/SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f9a2074f0c118ff8c931aeecb5abf6f9a14642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bfb3cd1979fed75720387676b0f8fb56c9cf63e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
