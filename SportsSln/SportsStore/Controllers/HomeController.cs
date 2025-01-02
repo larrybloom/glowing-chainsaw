c55dc0fa89cf4bfe50b7ae115b074d215cb07236
AniediAbasi Udoh <AniediAbasi.Udoh@sterling.ng>
2025-01-02 11:26:18 +0100

    pagination

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -4,10 +4,16 @@ using SportsStore.Models;
 namespace SportsStore.Controllers{
     public class HomeController : Controller{
         private IStoreRepository repository;
+        public int PageSize = 4;
         public HomeController(IStoreRepository repo){
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        public IActionResult Index(int productPage = 1) 
+            => View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
+                
     }
 
 }
