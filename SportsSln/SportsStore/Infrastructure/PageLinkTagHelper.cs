95fc7a8a0b918fe40c6f1fb69520efcf6311cf7d
AniediAbasi Udoh <AniediAbasi.Udoh@sterling.ng>
2025-01-06 09:52:49 +0100

    cart complete

--- SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -22,7 +22,7 @@ namespace SportsStore.Infrastructure{
         public string? PageAction {get; set;}
         [HtmlAttributeName(DictionaryAttributePrefix ="page-url-")]
         public Dictionary<string, object> PageUrlValues {get; set;}
-            = new Dictionary<string, object>();
+            =  new Dictionary<string, object>();
 
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; } = String.Empty;
