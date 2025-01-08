2c9534675076337035be618f3555e995047c14b1
AniediAbasi Udoh <AniediAbasi.Udoh@sterling.ng>
2025-01-08 17:15:52 +0100

    complete cart

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -3,32 +3,37 @@ using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 
 
-namespace SportsStore.Controllers{
-    public class HomeController : Controller{
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
         private IStoreRepository repository;
         public int PageSize = 4;
-        public HomeController(IStoreRepository repo){
+        public HomeController(IStoreRepository repo)
+        {
             repository = repo;
         }
-        public ViewResult Index(string? category, int productPage = 1) 
-            => View(new ProductsListViewModel {
+        public ViewResult Index(string? category, int productPage = 1)
+            => View(new ProductsListViewModel
+            {
                 Products = repository.Products
                 .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductId)
                 .Skip((productPage - 1) * PageSize)
                 .Take(PageSize),
-                PagingInfo = new PagingInfo{
+                PagingInfo = new PagingInfo
+                {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = category == null
                         ? repository.Products.Count()
                         : repository.Products.Where(e =>
                             e.Category == category).Count()
-                    },
-                    CurrentCategory = category
-                });
-                
-            }
-                
+                },
+                CurrentCategory = category
+            });
+
     }
+
+}
 
--- SportsSln/SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
+++ SportsSln/SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15e5d4c2171655737d9dea76c3d497e5a804f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95fc7a8a0b918fe40c6f1fb69520efcf6311cf7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
